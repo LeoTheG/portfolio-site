0fa24edf11aa417e53b5b4f10103ae1f864e9b0c
Leo Gharib <leonargharib@gmail.com>
2021-09-15 20:49:33 -0700

    update home page video width

--- src/components/DemoReel.js
+++ src/components/DemoReel.js
@@ -15,8 +15,9 @@ export default class DemoReel extends Component {
 
                 <Iframe url="https://player.vimeo.com/video/315374764?autoplay=1&loop=1"
                     // max-width="100%!important"
+					width="100%"
                     height={window.innerHeight * .8 + "px"}
-					style={{outerWidth: '100%'}}
+					// style={{outerWidth: '100%'}}
                     allowFullScreen
                     id="video"
                 />
