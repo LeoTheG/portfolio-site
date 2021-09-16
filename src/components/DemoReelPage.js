9bb781cb5b4c1c33f0c54c43f655c04c8440caa0
Leo Gharib <leonargharib@gmail.com>
2021-09-15 20:20:17 -0700

    add ci cd

--- src/components/DemoReelPage.js
+++ src/components/DemoReelPage.js
@@ -21,13 +21,13 @@ export default class DemoReelPage extends Component{
                     title: "Ocean",
                     description: "The scale of this shot extended render times but it was still done in reasonable time. I’m responsible for all aspects except modeling the ship. Everything was done in Houdini and composited in Nuke. "
                 },
-                "sword": {
-                    breakdown: "https://s3-us-west-1.amazonaws.com/gregkalamdaryanbucket2/website_files/videos/sword/sword_breakdown.mov",
-                    main: "https://s3-us-west-1.amazonaws.com/gregkalamdaryanbucket2/website_files/videos/sword/sword.mov",
-                    current: "main",
-                    title: "Sword",
-                    description: "This was done by making changes to an existing tool I had. In order to allow me to use materials on the objects, I had to delete the faces as opposed making their opacity 0 like I did on the other scenes. This was done in Houdini and composited in Nuke."
-                },
+                // "sword": {
+                //     breakdown: "https://s3-us-west-1.amazonaws.com/gregkalamdaryanbucket2/website_files/videos/sword/sword_breakdown.mov",
+                //     main: "https://s3-us-west-1.amazonaws.com/gregkalamdaryanbucket2/website_files/videos/sword/sword.mov",
+                //     current: "main",
+                //     title: "Sword",
+                //     description: "This was done by making changes to an existing tool I had. In order to allow me to use materials on the objects, I had to delete the faces as opposed making their opacity 0 like I did on the other scenes. This was done in Houdini and composited in Nuke."
+                // },
                 "tornado": {
                     breakdown: "https://s3-us-west-1.amazonaws.com/gregkalamdaryanbucket2/website_files/videos/tornado/tornado.mov",
                     main: "https://s3-us-west-1.amazonaws.com/gregkalamdaryanbucket2/website_files/videos/tornado/tornado_breakdown.mov",
